fa03b213c6388ad8365a501b50f86c9f898147bd
Sujin Kim <sujinkim919@gmail.com>
2023-02-17 02:41:18 +0900

    update: 컨텐츠 수정

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-document.getElementById("button_submit").onclick = function() {
+document.getElementById("button_submit").onclick = function () {
     const name = document.getElementById('visitor_name').value;
     const count = document.querySelectorAll('input[name="fav_movie"]:checked').length;
 
-    alert(name+"님, 저와 "+count+"개의 취향이 같으시네요!");
+    alert(name + "님, 저와 " + count + "개의 취향이 같으시네요!");
 }
